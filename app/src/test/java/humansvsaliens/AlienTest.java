e738101781cf27a1d413c19ff4d0cf18da946ba2
Alex <ajoshua14@icloud.com>
2023-03-10 14:08:37 -0800

    Swap human and alien

--- app/src/test/java/humansvsaliens/AlienTest.java
+++ app/src/test/java/humansvsaliens/AlienTest.java
@@ -7,31 +7,5 @@ import org.junit.jupiter.api.BeforeEach;
 import app.src.main.java.humansvsaliens.components.Alien;
 
 public class AlienTest {
-    String name;
-    int hp;
-    double weight;
-    Alien alien;
-
-    @BeforeEach
-    void createAlien() {
-        name = "Jill";
-        hp = 500;
-        weight = 100;
-        alien = new Alien(name, hp, weight);
-    }
-
-    @Test 
-    void alienHasName() {
-        assertEquals(name, alien.getName());
-    }
-
-    @Test 
-    void alienHasHP() {
-        assertEquals(hp, alien.getHP());
-    }
-
-    @Test
-    void alienHasWeight() {
-        assertEquals(weight, alien.getWeight());
-    }
+    
 }
--- app/src/test/java/humansvsaliens/HumanTest.java
+++ app/src/test/java/humansvsaliens/HumanTest.java
@@ -1,5 +1,37 @@
 package app.src.test.java.humansvsaliens;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import app.src.main.java.humansvsaliens.components.Human;
+
 public class HumanTest {
-    
+    String name;
+    int hp;
+    double weight;
+    Human human;
+
+    @BeforeEach
+    void createHuman() {
+        name = "Jill";
+        hp = 500;
+        weight = 100;
+        human = new Human(name, hp, weight);
+    }
+
+    @Test 
+    void humanHasName() {
+        assertEquals(name, human.getName());
+    }
+
+    @Test 
+    void humanHasHP() {
+        assertEquals(hp, human.getHP());
+    }
+
+    @Test
+    void humanHasWeight() {
+        assertEquals(weight, human.getWeight());
+    }
 }
