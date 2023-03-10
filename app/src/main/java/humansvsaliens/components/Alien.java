82722f291c9cc7e4a263c6a558ca5783d6a91b1d
Alex <ajoshua14@icloud.com>
2023-03-10 14:13:08 -0800

    Add power class

--- app/src/main/java/humansvsaliens/components/Alien.java
+++ app/src/main/java/humansvsaliens/components/Alien.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Alien extends Entity {
+    List<Power> powers;
+
+
     public Alien(String name, int hp, double weight) {
         super(name, hp, weight);
+        this.powers = new ArrayList<Power>();
     }
 }
